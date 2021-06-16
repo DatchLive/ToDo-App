ba331462b6637fc005d5151d1779685ae05c32d9
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-06-16 22:40:13 +0900

    ⚡️ ToDoアプリ機能実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+import styles from "src/styles/styles.module.css";
+
 export default function Home() {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "ああああ",
+    "いいいい",
+  ]);
+
+  const [completeTodos, setCompleteTodos] = useState(["うううう"]);
+
   return (
-  <div></div>
-  )
+    <>
+      <div className={styles.inputarea}>
+        <input placeholder="ToDoを入力" />
+        <button>追加</button>
+      </div>
+
+      <div className={styles.incompletearea}>
+        <p className={styles.title}>未完了のToDo</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className={styles.listrow}>
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className={styles.completearea}>
+        <p className={styles.title}>完了のToDo</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className={styles.listrow}>
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 }
