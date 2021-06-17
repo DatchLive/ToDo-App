77877ae2ca8dcb08f189656d170b4be3fa76565c
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-06-17 22:20:47 +0900

    ✨ タスク追加機能

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,18 +2,30 @@ import { useState } from "react";
 import styles from "src/styles/styles.module.css";
 
 export default function Home() {
-  const [incompleteTodos, setIncompleteTodos] = useState([
-    "ああああ",
-    "いいいい",
-  ]);
+  const [todoText, setTodoText] = useState("");
+
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
 
   const [completeTodos, setCompleteTodos] = useState(["うううう"]);
+
+  const onChangeTodoText = (e) => setTodoText(e.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
 
   return (
     <>
       <div className={styles.inputarea}>
-        <input placeholder="ToDoを入力" />
-        <button>追加</button>
+        <input
+          placeholder="ToDoを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
 
       <div className={styles.incompletearea}>
