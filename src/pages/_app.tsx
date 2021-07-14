d7ca88c4b4f30f5e24598824f90fd4632b258a0b
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-07-14 17:09:21 +0900

    ➕ Firebase環境設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
+import "src/libs/firebase";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
