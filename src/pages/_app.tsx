3e11f8decd7abf5d5ca8024e29c52294a15eefe2
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-07-17 20:25:17 +0900

    🐛 Firebase設定削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import "tailwindcss/tailwind.css";
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
-import "src/libs/firebase";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
